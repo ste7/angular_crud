bfdadc5ffaa78bbdbb1e6ce44291b1073371058a
Stefan <stefanbabicbg@gmail.com>
2018-11-10 10:08:14 +0100

    api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,58 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePostRequest;
 
 class PostController extends Controller
 {
-     
+    /**
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function index() {
+        return response([
+            'posts' => Post::all()
+        ], 200);
+    }
+
+    /**
+     * @param Post $post
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Post $post) {
+        return response()->json([
+            'post' => $post->load('user')
+        ], 200);
+    }
+
+    /**
+     * @param CreatePostRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(CreatePostRequest $request) {
+        $post = Post::create($request->all());
+
+        return response()->json([
+            'post' => $post
+        ], 200);
+    }
+
+    /**
+     * @param Post $post
+     */
+    public function destroy(Post $post) {
+        $post->delete();
+    }
+
+    /**
+     * @param CreatePostRequest $request
+     * @param Post $post
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function update(CreatePostRequest $request, Post $post) {
+        return response([
+            'post' => tap($post)->update($request->all())
+        ], 200);
+    }
 }
